2f68e8b64e9a9469b8c28f10cccaa49df994d2f9
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2024-03-02 16:38:17 +0500

    commit

--- PcTgBot/Commands/Screenshot.cs
+++ PcTgBot/Commands/Screenshot.cs
@@ -13,31 +13,13 @@ namespace PcTgBot.Commands
 {
     internal class Screenshot
     {
-        private Bitmap ResizeImg(Bitmap bmp, int nWidth, int nHeight)
-        {
-            var result = new Bitmap(nWidth, nHeight);
-
-            using (var g = Graphics.FromImage(result))
-            {
-                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                g.DrawImage(bmp, 0, 0, nWidth, nHeight);
-            }
-
-            return result;
-        }
-
-        private Bitmap ResizeImg(Bitmap bmp, int xN)
-        {
-            return ResizeImg(bmp, bmp.Width / xN, bmp.Height / xN);
-        }
-
         private Bitmap GetPrintScreen()
         {
             var screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             var graphics = Graphics.FromImage(screen);
             graphics.CopyFromScreen(0, 0, 0, 0, screen.Size);
 
-            return ResizeImg(screen, 2);
+            return screen;
         }
 
         public async Task SendPrintScreen(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 ## Telegram Bot for interacting with PC
 
 Before you start, please provide token and user id in appsettings.json.
+If there's no appsettings.json then create it in project folder (folder where .csproj file located).
+It should be as the following:
+```
+{
+  "Creds": {
+    "BotToken": "", // Your bot's token which you can get from https://t.me/BotFather
+    "UserId": 0 // You can get your user id using bots such as https://t.me/userinfobot
+  }
+}
+```
